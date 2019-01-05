a5dcaa82be0ffcbc5de2400373b0a59685dac02b
Darius Goore <dariusgoore@gmail.com>
2019-01-04 21:27:55 -0500

    adding view at home to display index

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,58 +1,46 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <hr>
+    <div class="list-unstyled" v-for="message in messages" :key="message._id">
+      <li class="media">
+        <img v-if="message.imageURL" class="mr-3" :src="message.imageURL" :alt="message.subject">
+        <div class="media-body">
+          <h4 class="mt-0 mb-1">{{message.username}}</h4>
+          <h5 class="mt-0 mb-1">{{message.subject}}</h5>
+          {{message.message}}
+          <br />
+          <small>{{message.created}}</small>
+        </div>
+      </li>
+      <hr>
+    </div>
   </div>
 </template>
 
 <script>
+const API_URL = "http://localhost:4000/messages";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
+  name: "home",
+  data: () => ({
+    error: "",
+    messages: []
+  }),
+
+  mounted() {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(result => {
+        this.messages = result;
+      });
   },
+  methods: {}
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+img {
+  max-width: 300px;
+  height: auto;
 }
 </style>
